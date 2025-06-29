8b11da8bd49be82df9093af3a30ec2d6698c4dd7
Ojas <ojaskul96@gmail.com>
2025-06-28 23:40:06 -0500

    delete temp debug code

--- mcp_server/main.py
+++ mcp_server/main.py
@@ -1,17 +1,11 @@
 # mcp_server/main.py
 
 import os
-os.environ["PYLGTV_STORE"] = "/Users/ojaskulkarni/dev/mcp-server-project/lgtv-mcp-experiment/.aiopylgtv.sqlite"
 
 from fastmcp import FastMCP
 from tools.tv_controller.netflix_controller import play_netflix
 from typing import Annotated
 from pydantic import Field
-
-
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-# Explicit override BEFORE importing any pylgtv/webos modules
-
 
 mcp = FastMCP(name="Media Assistant")
 
